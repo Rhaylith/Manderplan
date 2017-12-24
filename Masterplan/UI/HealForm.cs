df018101eda51f401c4b01c2e69edccd71e325b7
Mark Andersen <Mark Andersen>
2017-12-23 19:42:09 -0800

    Fixing the damn heal window!

--- Masterplan/UI/HealForm.cs
+++ Masterplan/UI/HealForm.cs
@@ -37,7 +37,7 @@ namespace Masterplan.UI
 
 		private void DamageForm_Shown(object sender, EventArgs e)
 		{
-			this.SurgeBox.Select(0, 1);
+			this.HPBox.Select(0, 1);
 		}
 
 		protected override void Dispose(bool disposing)
@@ -51,93 +51,124 @@ namespace Masterplan.UI
 
 		private void InitializeComponent()
 		{
-			this.OKBtn = new Button();
-			this.CancelBtn = new Button();
-			this.HPLbl = new Label();
-			this.HPBox = new NumericUpDown();
-			this.TempHPBox = new CheckBox();
-			this.SurgeLbl = new Label();
-			this.SurgeBox = new NumericUpDown();
-			((ISupportInitialize)this.HPBox).BeginInit();
-			((ISupportInitialize)this.SurgeBox).BeginInit();
-			base.SuspendLayout();
-			this.OKBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-			this.OKBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.OKBtn.Location = new Point(116, 110);
-			this.OKBtn.Name = "OKBtn";
-			this.OKBtn.Size = new System.Drawing.Size(75, 23);
-			this.OKBtn.TabIndex = 5;
-			this.OKBtn.Text = "OK";
-			this.OKBtn.UseVisualStyleBackColor = true;
-			this.OKBtn.Click += new EventHandler(this.OKBtn_Click);
-			this.CancelBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-			this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.CancelBtn.Location = new Point(197, 110);
-			this.CancelBtn.Name = "CancelBtn";
-			this.CancelBtn.Size = new System.Drawing.Size(75, 23);
-			this.CancelBtn.TabIndex = 6;
-			this.CancelBtn.Text = "Cancel";
-			this.CancelBtn.UseVisualStyleBackColor = true;
-			this.HPLbl.AutoSize = true;
-			this.HPLbl.Location = new Point(12, 40);
-			this.HPLbl.Name = "HPLbl";
-			this.HPLbl.Size = new System.Drawing.Size(25, 13);
-			this.HPLbl.TabIndex = 2;
-			this.HPLbl.Text = "HP:";
-			this.HPBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-			this.HPBox.Location = new Point(68, 38);
-			NumericUpDown hPBox = this.HPBox;
-			int[] numArray = new int[] { 1000, 0, 0, 0 };
-			hPBox.Maximum = new decimal(numArray);
-			this.HPBox.Name = "HPBox";
-			this.HPBox.Size = new System.Drawing.Size(204, 20);
-			this.HPBox.TabIndex = 3;
-			this.TempHPBox.AutoSize = true;
-			this.TempHPBox.Location = new Point(68, 75);
-			this.TempHPBox.Name = "TempHPBox";
-			this.TempHPBox.Size = new System.Drawing.Size(153, 17);
-			this.TempHPBox.TabIndex = 4;
-			this.TempHPBox.Text = "Add as temporary hit points";
-			this.TempHPBox.UseVisualStyleBackColor = true;
-			this.SurgeLbl.AutoSize = true;
-			this.SurgeLbl.Location = new Point(12, 14);
-			this.SurgeLbl.Name = "SurgeLbl";
-			this.SurgeLbl.Size = new System.Drawing.Size(43, 13);
-			this.SurgeLbl.TabIndex = 0;
-			this.SurgeLbl.Text = "Surges:";
-			this.SurgeBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-			this.SurgeBox.Location = new Point(68, 12);
-			NumericUpDown surgeBox = this.SurgeBox;
-			int[] numArray1 = new int[] { 10, 0, 0, 0 };
-			surgeBox.Maximum = new decimal(numArray1);
-			this.SurgeBox.Name = "SurgeBox";
-			this.SurgeBox.Size = new System.Drawing.Size(204, 20);
-			this.SurgeBox.TabIndex = 1;
-			base.AcceptButton = this.OKBtn;
-			base.AutoScaleDimensions = new SizeF(6f, 13f);
-			base.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			base.CancelButton = this.CancelBtn;
-			base.ClientSize = new System.Drawing.Size(284, 145);
-			base.Controls.Add(this.SurgeBox);
-			base.Controls.Add(this.SurgeLbl);
-			base.Controls.Add(this.TempHPBox);
-			base.Controls.Add(this.HPBox);
-			base.Controls.Add(this.HPLbl);
-			base.Controls.Add(this.CancelBtn);
-			base.Controls.Add(this.OKBtn);
-			base.MaximizeBox = false;
-			base.MinimizeBox = false;
-			base.Name = "HealForm";
-			base.ShowIcon = false;
-			base.ShowInTaskbar = false;
-			base.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-			base.StartPosition = FormStartPosition.CenterParent;
-			this.Text = "Heal";
-			base.Shown += new EventHandler(this.DamageForm_Shown);
-			((ISupportInitialize)this.HPBox).EndInit();
-			((ISupportInitialize)this.SurgeBox).EndInit();
-			base.ResumeLayout(false);
-			base.PerformLayout();
+            this.OKBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.HPLbl = new System.Windows.Forms.Label();
+            this.HPBox = new System.Windows.Forms.NumericUpDown();
+            this.TempHPBox = new System.Windows.Forms.CheckBox();
+            this.SurgeLbl = new System.Windows.Forms.Label();
+            this.SurgeBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.HPBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SurgeBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OKBtn
+            // 
+            this.OKBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OKBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OKBtn.Location = new System.Drawing.Point(116, 110);
+            this.OKBtn.Name = "OKBtn";
+            this.OKBtn.Size = new System.Drawing.Size(75, 23);
+            this.OKBtn.TabIndex = 4;
+            this.OKBtn.Text = "OK";
+            this.OKBtn.UseVisualStyleBackColor = true;
+            this.OKBtn.Click += new System.EventHandler(this.OKBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelBtn.Location = new System.Drawing.Point(197, 110);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 5;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // HPLbl
+            // 
+            this.HPLbl.AutoSize = true;
+            this.HPLbl.Location = new System.Drawing.Point(12, 14);
+            this.HPLbl.Name = "HPLbl";
+            this.HPLbl.Size = new System.Drawing.Size(25, 13);
+            this.HPLbl.TabIndex = 0;
+            this.HPLbl.Text = "HP:";
+            // 
+            // HPBox
+            // 
+            this.HPBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HPBox.Location = new System.Drawing.Point(68, 12);
+            this.HPBox.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.HPBox.Name = "HPBox";
+            this.HPBox.Size = new System.Drawing.Size(204, 20);
+            this.HPBox.TabIndex = 1;
+            // 
+            // TempHPBox
+            // 
+            this.TempHPBox.AutoSize = true;
+            this.TempHPBox.Location = new System.Drawing.Point(68, 75);
+            this.TempHPBox.Name = "TempHPBox";
+            this.TempHPBox.Size = new System.Drawing.Size(153, 17);
+            this.TempHPBox.TabIndex = 3;
+            this.TempHPBox.Text = "Add as temporary hit points";
+            this.TempHPBox.UseVisualStyleBackColor = true;
+            // 
+            // SurgeLbl
+            // 
+            this.SurgeLbl.AutoSize = true;
+            this.SurgeLbl.Location = new System.Drawing.Point(12, 40);
+            this.SurgeLbl.Name = "SurgeLbl";
+            this.SurgeLbl.Size = new System.Drawing.Size(43, 13);
+            this.SurgeLbl.TabIndex = 0;
+            this.SurgeLbl.Text = "Surges:";
+            // 
+            // SurgeBox
+            // 
+            this.SurgeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SurgeBox.Location = new System.Drawing.Point(68, 38);
+            this.SurgeBox.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.SurgeBox.Name = "SurgeBox";
+            this.SurgeBox.Size = new System.Drawing.Size(204, 20);
+            this.SurgeBox.TabIndex = 2;
+            // 
+            // HealForm
+            // 
+            this.AcceptButton = this.OKBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelBtn;
+            this.ClientSize = new System.Drawing.Size(284, 145);
+            this.Controls.Add(this.SurgeBox);
+            this.Controls.Add(this.SurgeLbl);
+            this.Controls.Add(this.TempHPBox);
+            this.Controls.Add(this.HPBox);
+            this.Controls.Add(this.HPLbl);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.OKBtn);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HealForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Heal";
+            this.Shown += new System.EventHandler(this.DamageForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.HPBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SurgeBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
 		private void OKBtn_Click(object sender, EventArgs e)
